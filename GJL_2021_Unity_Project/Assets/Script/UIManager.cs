2f0a5eb4ece6a1163be039dac34c638c774ec12b
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-07-13 22:03:25 +0100

    Armour and Item management

--- GJL_2021_Unity_Project/Assets/Script/UIManager.cs
+++ GJL_2021_Unity_Project/Assets/Script/UIManager.cs
@@ -7,14 +7,34 @@ public class UIManager : MonoBehaviour
 {
     public Text lblHealth;
     public Text lblSpeed;
+    public Text lblArmour;
+
+    public Text lblItemQueue;
+
+    static UIManager instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
     
     // Start is called before the first frame update
     void Start()
     {
         SetHealth(Player.Instance.Health);
         SetSpeed(Player.Instance.Speed);
+        SetArmour(0);
+
         Player.Instance.HealthChanged += SetHealth;
         Player.Instance.SpeedChanged += SetSpeed;
+        Player.Instance.ArmourChanged += SetArmour;
     }
 
     void SetHealth(int health)
@@ -38,6 +58,34 @@ public class UIManager : MonoBehaviour
         else
         {
             Debug.Log("lblSpeed is null you absolute nob head.");
+        }
+    }
+
+    void SetArmour(int armour)
+    {
+        if (lblArmour != null)
+        {
+            lblArmour.text = "Armour: "+armour;
+        }
+        else
+        {
+            Debug.Log("lblArmour is non-existent :/");
+        }
+    }
+
+    List<string> items= new List<string>();
+
+    public void AddItem(Item item)
+    {
+        items.Add(item.gameObject.name);
+        if (lblItemQueue != null)
+        {
+            string text = "Item Queue: ";
+            foreach(string itemStr in items)
+            {
+                text += itemStr + ",";
+            }
+            lblItemQueue.text = text;
         }
     }
 
