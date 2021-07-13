7cb3ff049eb6be9647cd89ace4cb9eca1d05791e
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-07-13 12:35:07 +0100

    Created "LoadScene" function which loads a specific scene enum value.

--- GJL_2021_Unity_Project/Assets/Script/Preliminary Architecture Stuff/Util.cs
+++ GJL_2021_Unity_Project/Assets/Script/Preliminary Architecture Stuff/Util.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public static class Util
 {
     public static string NormaliseString(this object obj)
     {
         return obj.ToString().Trim().ToLower();
+    }
+
+    public static void LoadScene(SCENE scene, LoadSceneMode mode = LoadSceneMode.Single)
+    {
+        SceneManager.LoadSceneAsync((int)scene, mode);
     }
 }
