c3e726487dd46ff12c6da275744d16f4405ee62f
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-07-15 18:12:56 +0100

    Implemented singleton and returning "CameraGroundForwward"

--- GJL_2021_Unity_Project/Assets/Script/CameraFollow.cs
+++ GJL_2021_Unity_Project/Assets/Script/CameraFollow.cs
@@ -4,6 +4,29 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    static CameraFollow instance;
+    public static CameraFollow Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public Vector3 CameraGroundForward
+    {
+        get
+        {
+            Vector3 orientation = transform.rotation.eulerAngles;
+            return new Vector3(0, orientation.y, 0).normalized;
+        }
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+
     public Vector3 intercept = new Vector3(-3.4f, 6.82f, -3.24f);
 
     Vector3 PlayerPosition
