b0320f4f4624e90b7e7a5fbca2ea0fa838a42a97
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-07-17 12:38:15 +0100

    Integrating Attack() and StopAttack() with previous shooting code

--- GJL_2021_Unity_Project/Assets/Script/Enemies/RangedEnemy.cs
+++ GJL_2021_Unity_Project/Assets/Script/Enemies/RangedEnemy.cs
@@ -1,7 +1,8 @@
- using UnityEngine;
+using System.Collections;
+using UnityEngine;
 
 public class RangedEnemy : Enemy {   
-    const float range = 10f;
+    //const float range = 10f;
     float distanceToPlayer = -1;
 
     const float fireRate = 1f; // rate of fire
@@ -12,34 +13,58 @@ public class RangedEnemy : Enemy {
     private float nextFire = 0f;
 
     private void Update() {
-        distanceToPlayer = Vector3.Distance(playerGO.transform.position, transform.position);
+        // distanceToPlayer = Vector3.Distance(playerGO.transform.position, transform.position);
 
-        if(distanceToPlayer <= range) // within range, shoot
-        {
-            ShootWeapon();
-        }
+        // if(distanceToPlayer <= range) // within range, shoot
+        // {
+        //     ShootWeapon();
+        // }
     }
     
 
-    private void ShootWeapon()
+    private float ShootWeapon()
     {
-        if (Time.time > nextFire)
-        {
-            if(shotsRemaining <= 0)
-            {
-                nextFire = Time.time + reloadTime;
-                shotsRemaining = clipSize;
-                Debug.LogFormat("Reloading for {0}s", reloadTime);
-            } else
-            {
-                nextFire = Time.time + fireRate;
-                shotsRemaining--;
-                // TODO handle actual firing/damage to player
-                Debug.LogFormat("BANG ! Fired Weapon. {0}/{1} Shots remaining", shotsRemaining, clipSize);
-            }
+        // if (Time.time > nextFire)
+        // {
+
 
             
+        // }
+
+        if(shotsRemaining <= 0)
+        {
+            //nextFire = Time.time + reloadTime;
+            shotsRemaining = clipSize;
+            Debug.LogFormat("Reloading for {0}s", reloadTime);
+            return reloadTime;
+        } else
+        {
+            //nextFire = Time.time + fireRate;
+            shotsRemaining--;
+            // TODO handle actual firing/damage to player
+            Debug.LogFormat("BANG ! Fired Weapon. {0}/{1} Shots remaining", shotsRemaining, clipSize);
+            return fireRate;
         }
     }
 
+    bool shooting = false;
+
+    IEnumerator Shooting()
+    {
+        shooting = true;
+        while(shooting)
+        {
+            yield return new WaitForSeconds(ShootWeapon());
+        }
+    }
+
+    protected override void Attack()
+    {
+        StartCoroutine(Shooting());
+    }
+
+    protected override void StopAttack()
+    {
+        shooting = false;
+    }
 }
