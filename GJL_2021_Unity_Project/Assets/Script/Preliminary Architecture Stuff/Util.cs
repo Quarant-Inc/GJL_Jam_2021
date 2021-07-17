f3d2802562edce019e5c7bbc177c70595ea6a70d
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-07-17 10:57:09 +0100

    Created function for getting random available navmesh position

--- GJL_2021_Unity_Project/Assets/Script/Preliminary Architecture Stuff/Util.cs
+++ GJL_2021_Unity_Project/Assets/Script/Preliminary Architecture Stuff/Util.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 public static class Util
@@ -28,4 +29,39 @@ public static class Util
         dir = new Vector3();
         return false;
     }
+
+    //https://answers.unity.com/questions/857827/pick-random-point-on-navmesh.html
+     public static Vector3 GetRandomNavMeshLocation()
+     {
+         NavMeshTriangulation navMeshData = NavMesh.CalculateTriangulation();
+ 
+         int maxIndices = navMeshData.indices.Length - 3;
+         // Pick the first indice of a random triangle in the nav mesh
+         int firstVertexSelected = Random.Range(0, maxIndices);
+         int secondVertexSelected = Random.Range(0, maxIndices);
+         //Spawn on Verticies
+         Vector3 point = navMeshData.vertices[navMeshData.indices[firstVertexSelected]];
+ 
+         Vector3 firstVertexPosition = navMeshData.vertices[navMeshData.indices[firstVertexSelected]];
+         Vector3 secondVertexPosition = navMeshData.vertices[navMeshData.indices[secondVertexSelected]];
+         //Eliminate points that share a similar X or Z position to stop spawining in square grid line formations
+         if ((int)firstVertexPosition.x == (int)secondVertexPosition.x ||
+             (int)firstVertexPosition.z == (int)secondVertexPosition.z
+             )
+         {
+             point = GetRandomNavMeshLocation(); //Re-Roll a position - I'm not happy with this recursion it could be better
+         }
+         else
+         {
+             // Select a random point on it
+             point = Vector3.Lerp(
+                                             firstVertexPosition,
+                                             secondVertexPosition, //[t + 1]],
+                                             Random.Range(0.05f, 0.95f) // Not using Random.value as clumps form around Verticies 
+                                         );
+         }
+         //Vector3.Lerp(point, navMeshData.vertices[navMeshData.indices[t + 2]], Random.value); //Made Obsolete
+ 
+         return point;
+     }
 }
