9fe8209c3c173fa9cc89922ff4565c483f6a4b10
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-07-17 12:31:04 +0100

    Created behaviour logic loop

--- GJL_2021_Unity_Project/Assets/Script/Enemies/Enemy.cs
+++ GJL_2021_Unity_Project/Assets/Script/Enemies/Enemy.cs
@@ -1,8 +1,9 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
-public class Enemy : MonoBehaviour
+public abstract class Enemy : MonoBehaviour
 {
     NavMeshAgent agent;
     protected NavMeshAgent Agent
@@ -16,7 +17,14 @@ public class Enemy : MonoBehaviour
             return agent;
         }
     }
-    public GameObject playerGO;
+
+    Vector3 PlayerPosition
+    {
+        get
+        {
+            return Player.Instance.transform.position;
+        }
+    }
     
     // some of these might be better public idk
     const int  maxHealth = 1;
@@ -25,11 +33,22 @@ public class Enemy : MonoBehaviour
     int health = maxHealth;
     public int armour = 0;
 
-    private void Awake()  {
-        playerGO = GameObject.Find("Player");
-        if(playerGO == null){
-            Debug.Log("Player not found by name");
-        }
+    public float sightRadius = 10f;
+    public float attackRange;
+
+    enum ENEMY_STATE
+    {
+        IDLE,
+        CHASING,
+        SEARCHING,
+        ATTACKING
+    }
+
+    ENEMY_STATE currentState = ENEMY_STATE.IDLE;
+
+    private void Awake()
+    {
+
     }
 
     void Update()    {
@@ -56,5 +75,88 @@ public class Enemy : MonoBehaviour
     private void KillEnemy(){
         // do other killing stuff here
         Destroy(gameObject);
+    }
+
+    protected abstract void Attack();
+    protected abstract void StopAttack();
+
+    Vector3 lastSeenPos;
+
+    void StateCheck()
+    {
+        switch(currentState)
+        {
+            case ENEMY_STATE.IDLE:
+            {
+                if (PlayerVisible())
+                {
+                    lastSeenPos = PlayerPosition;
+                    currentState = ENEMY_STATE.CHASING;
+                }
+                break;
+            }
+            case ENEMY_STATE.CHASING:
+            {
+                if (!PlayerVisible())
+                {
+                    currentState = ENEMY_STATE.SEARCHING;
+                }
+                else
+                {
+                    lastSeenPos = PlayerPosition;
+                    Agent.SetDestination(lastSeenPos);
+                    if (PlayerWithinAttackRange())
+                    {
+                        currentState = ENEMY_STATE.ATTACKING;
+                        Attack();
+                    }
+                }
+                break;
+            }
+            case ENEMY_STATE.SEARCHING:
+            {
+                if (PlayerVisible())
+                {
+                    currentState = ENEMY_STATE.CHASING;
+                    lastSeenPos = PlayerPosition;
+                }
+                else if (Vector3.Distance(lastSeenPos, transform.position) < 1.5f)
+                {
+                    currentState = ENEMY_STATE.IDLE;
+                }
+                break;
+            }
+            case ENEMY_STATE.ATTACKING:
+            {
+                if (!PlayerWithinAttackRange())
+                {
+                    currentState = ENEMY_STATE.CHASING;
+                    StopAttack();
+                }
+                break;
+            }
+        }
+    }
+
+    bool PlayerVisible()
+    {
+        return Vector3.Distance(transform.position, PlayerPosition) <= sightRadius;
+    }
+
+    bool PlayerWithinAttackRange()
+    {
+        return Vector3.Distance(transform.position, PlayerPosition) <= attackRange;
+    }
+
+    bool checkingForPlayer = true;
+
+    IEnumerator CheckForPlayer()
+    {
+        //This desperately needs to be optimised.
+        while(checkingForPlayer)
+        {
+
+            yield return 0;
+        }
     }
 }
