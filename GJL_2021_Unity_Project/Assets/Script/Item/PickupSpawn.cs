254785b329a241e3481c9a2d50bceffe3cac625f
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-07-17 11:12:16 +0100

    Created script for spawning pickup items

---
+++ GJL_2021_Unity_Project/Assets/Script/Item/PickupSpawn.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawn : MonoBehaviour
+{
+    public GameObject[] pickupPrefabs;
+
+    public uint itemQuantity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (uint i = 0; i < itemQuantity; i++)
+        {
+            Vector3 pos = Util.GetRandomNavMeshLocation();
+            pos.y += 0.5f;
+            GameObject prefab = pickupPrefabs.GetRandomElement();
+            GameObject item = Instantiate(prefab,pos, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
