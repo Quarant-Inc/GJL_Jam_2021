18fdaead09a529a7766ef61e0ff869295e984a66
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-07-17 12:49:56 +0100

    Created attack loop

--- GJL_2021_Unity_Project/Assets/Script/Enemies/MeleeEnemy.cs
+++ GJL_2021_Unity_Project/Assets/Script/Enemies/MeleeEnemy.cs
@@ -1,8 +1,27 @@
+using System.Collections;
 using UnityEngine;
 
 public class MeleeEnemy : Enemy
 {
-    public float attackDamage;
-    public float detectionRange;
     public float attackSpeed;
+
+    bool attacking = false;
+
+    IEnumerator AttackLoop()
+    {
+        attacking = true;
+        while(attacking)
+        {
+            yield return new WaitForSeconds(1f/attackSpeed);
+        }
+    }
+
+    protected override void Attack()
+    {
+        StartCoroutine(AttackLoop());
+    }
+    protected override void StopAttack()
+    {
+        attacking = false;
+    }
 }
